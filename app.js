00431b1053838ae25a4311cd2779a1855aaf2aae
Muhammad Bilal Khan-MBK <kahnbilal2011@gmail.com>
2024-08-09 12:24:10 +0500

    added drop downs

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
 const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@{date}/v1/{endpoint}";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+
+for (let select of dropdowns){
+    for (let currCode in countryList){
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+        if (select.name === "from" && currCode === "PKR"){
+            newOption.selected = "selected";
+        } else if (select.name === "to" && currCode === "USD"){
+            newOption.selected = "selected";
+        }
+        select.append(newOption);
+    }
+
+    select.addEventListener("change", (evt) => {
+        updateFlag(evt.target);
+    });
+}
+
+const updateFlag = (element) => {
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img = element.parentElement.querySelector("img");
+    img.src = newSrc;
+}
+
+btn.addEventListener("click", async (evt) => {
+    evt.preventDefault();
+    let amount = document.querySelector(".amount input");
+    let amtValue = amount.value;
+    if (amtValue === "" || amtValue < 1){
+        amtValue = 1;
+        amount.value = "1";
+    }
+
+    // console.log(fromCurr.value,toCurr.value);
+
+    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+    let response = await fetch(URL);
+    console.log(response);
+});
